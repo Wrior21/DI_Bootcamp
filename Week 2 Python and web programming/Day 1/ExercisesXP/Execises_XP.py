0329e5f61c949812bd0c3aad5f2a9b3a0081dc6e
Wrior21 <shimonstern619@gmail.com>
2023-07-16 23:18:07 +0300

    the first exercise

---
+++ Week 2 Python and web programming/Day 1/ExercisesXP/Execises_XP.py
@@ -0,0 +1,54 @@
+Exercise_XP.complete 
+
+# Ex 1: hello World
+
+a=36
+b=15
+if a>b:
+print ('hello world')*5 
+
+#Ex 2: Some Math
+
+#index(sum)
+# The calculation adds up to the sum of:
+//*# the line of the sum is =
+  .sum ('(99^3)*8')
+
+
+#Ex 3: What is the output
+#Determine the output of the snippets
+print (5 < 3) is of the solution 
+False 
+print (3==3) is of the solution 
+True 
+print (3 == "3") is of the solution
+False
+print ('3'>3) is of the solution 
+False
+print ('hello' == 'hello') is of the solution
+True
+#Ex 4: Your computer Brand
+
+Computer Brand is love the p tag connects with the z tag and creating infinite flow.
+
+x="lenovo"  
+                                # //*x is a computer brand that is branding of the method.
+                                 # //*x is now of a int in the shell goes into format of running.
+
+
+ print("I have a"+ x + "computer")
+
+
+# Ex 5: Your Information
+  
+# EX 6: A & B
+
+#Ex 7: Odd or even
+
+#Ex8: Whats your name?
+
+#Ex 9: Tall Enough to ride a Roller Coaster: 
+
+  
+
+
